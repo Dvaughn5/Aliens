e676f630f0800505a18e51b69190a78c00c9e81c
Dvaughn5 <vaughndorian@gmail.com>
2022-01-17 13:42:59 -0500

    First fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,7 +74,17 @@ class AlienInvasion:
 	def _create_fleet(self):
 		# Creating the first flet of aliens
 		alien = Alien(self)
-		self.aliens.add(alien)
+		alien_width = alien.rect.width
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = available_space_x // (2 * alien_width)
+
+		# Create the first row of aliens
+		for alien_num in range(number_aliens_x):
+			# Create an alien and place it in the row 
+			alien = Alien(self)
+			alien.x = alien_width + 2 * alien_width * alien_num
+			alien.rect.x = alien.x 
+			self.aliens.add(alien)
 
 	def _update_bullets(self):
 		# Update the position of bullets and get rid of old bullets 
